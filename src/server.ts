7b68d3cac857b64a9b0ed01dcfc2e5fc0e4cb8a3
Vijay Nimar <vijaynimar8@gmail.com>
2025-09-24 17:01:02 +0530

    update port

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,8 @@ const app=express()
 // app.use(express.json())
 app.use(bodyParser.json({ limit: '50mb' }));
 
-const PORT=8000
-const HOST=String(process.env.HOST)||"localhost"
+const PORT=parseInt(process.env.PORT ?? "8000", 10)
 app.use("/api",router)
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
     console.log(`server started at port ${PORT}`);
 })
