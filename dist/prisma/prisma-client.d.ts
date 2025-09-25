7ae31411e9892c593b0ca6e23797d709c7befb60
Vijay Nimar <vijaynimar8@gmail.com>
2025-09-25 11:51:29 +0530

    update project

--- dist/prisma/prisma-client.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-export declare const getPrismaClient: () => PrismaClient;
-export type IPrismaClient = PrismaClient;
-//# sourceMappingURL=prisma-client.d.ts.map
